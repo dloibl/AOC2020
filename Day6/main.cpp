207316299914ad6dae1c55146c9271d5cdd8e890
Daniel Loibl <daniel.loibl@gmail.com>
2020-12-06 12:50:00 +0100

    Day6: solve part 2

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,25 +1,43 @@
 #include "../utils.cpp"
 #include <set>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-    int yesAnswers = 0;
-    set<char> answersInGroup;
+    int numberAnyoneAnsweredYes = 0;
+    int numberEveryoneAnsweredYes = 0;
+    map<char, int> answers;
+    int numberPersonsInGroup = 0;
     for (string line : readData("./input.txt"))
     {
         if (line.length() == 0)
         {
-            yesAnswers += answersInGroup.size();
-            answersInGroup.clear();
+            numberAnyoneAnsweredYes += answers.size();
+
+            for (map<char, int>::iterator i = answers.begin(); i != answers.end(); i++)
+            {
+                if (i->second == numberPersonsInGroup)
+                    numberEveryoneAnsweredYes++;
+            }
+
+            answers.clear();
+            numberPersonsInGroup = 0;
         }
         else
         {
             for (char answer : line)
-                answersInGroup.insert(answer);
+            {
+                map<char, int>::iterator it = answers.find(answer);
+                if (it == answers.end())
+                    answers.insert(pair<char, int>(answer, 1));
+                it->second++;
+            }
+            numberPersonsInGroup++;
         }
     }
 
-    cout << "the answer to part 1 is: " << yesAnswers << endl;
+    cout << "the answer to part 1 is: " << numberAnyoneAnsweredYes << endl;
+    cout << "the answer to part 2 is: " << numberEveryoneAnsweredYes << endl;
 }
