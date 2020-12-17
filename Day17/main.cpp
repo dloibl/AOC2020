6f22ef509278f07dc18dc3b4364913b55526604d
Daniel Loibl <daniel.loibl@gmail.com>
2020-12-17 20:00:26 +0100

    Day17: solve part2

--- Day17/main.cpp
+++ Day17/main.cpp
@@ -4,16 +4,17 @@
 
 using namespace std;
 
-typedef array<array<array<bool, 20>, 20>, 20> Cube;
+typedef array<array<array<array<bool, 20>, 20>, 20>, 20> Cube;
 
-int countActiveNeighbors(Cube &cube, int &x, int &y, int &z)
+int countActiveNeighbors(Cube &cube, int &x, int &y, int &z, int &w)
 {
     int count = 0;
     for (int i = z - 1; i <= z + 1; i++)
         for (int j = x - 1; j <= x + 1; j++)
             for (int l = y - 1; l <= y + 1; l++)
-                if (!(i == z && j == x && l == y) && cube[i][j][l] == 1)
-                    count++;
+                for (int k = w - 1; k <= w + 1; k++)
+                    if (!(i == z && j == x && l == y && k == w) && cube[i][k][j][l] == 1)
+                        count++;
     return count;
 }
 
@@ -27,28 +28,25 @@ int main()
     int offset = startS / 2;
     Cube cube;
     Cube next;
+
     for (int z = 0; z < N; z++)
-    {
         for (int x = 0; x < N; x++)
-        {
             for (int y = 0; y < N; y++)
-            {
-                cube[z][x][y] = 0;
-                next[z][x][y] = 0;
-            }
-        }
-    }
+                for (int w = 0; w < N; w++)
+                {
+                    cube[z][x][y][w] = 0;
+                    next[z][x][y][w] = 0;
+                }
 
     int activeNeighbors = 0;
     int a = 0;
     for (auto line : input)
     {
-        cout << line << endl;
         for (int b = 0; b < line.size(); b++)
         {
             if (line[b] == '#')
             {
-                cube[center][b + (center - offset)][a + (center - offset)] = 1;
+                cube[center][center][b + (center - offset)][a + (center - offset)] = 1;
             }
         }
         a++;
@@ -56,39 +54,36 @@ int main()
 
     for (int cycle = 1; cycle < 7; cycle++)
     {
-        //cout << "cycle=" << cycle << endl<< endl;
         for (int z = center - cycle; z <= center + cycle; z++)
         {
-            //cout << "z=" << z << endl;
-            for (int y = center - offset - cycle; y <= center + offset + cycle; y++)
+            for (int w = center - cycle; w <= center + cycle; w++)
             {
-                for (int x = center - offset - cycle; x <= center + offset + cycle; x++)
+                for (int y = center - offset - cycle; y <= center + offset + cycle; y++)
                 {
-                    cout << (cube[z][x][y] ? '#' : '.');
-
-                    activeNeighbors = countActiveNeighbors(cube, x, y, z);
-                    //cout << "z,x,y" << z << "," << x << "," << y << " #=" << cube[z][x][y] << "  n:" << activeNeighbors << endl;
-
-                    if (cube[z][x][y])
+                    for (int x = center - offset - cycle; x <= center + offset + cycle; x++)
                     {
-                        if (activeNeighbors == 3 || activeNeighbors == 2)
-                            next[z][x][y] = 1;
-                        else
+                        activeNeighbors = countActiveNeighbors(cube, x, y, z, w);
+
+                        if (cube[z][w][x][y])
                         {
-                            next[z][x][y] = 0;
-                        }
-                    }
-                    else
-                    {
-                        if (activeNeighbors == 3)
-                        {
-                            next[z][x][y] = 1;
+                            if (activeNeighbors == 3 || activeNeighbors == 2)
+                                next[z][w][x][y] = 1;
+                            else
+                            {
+                                next[z][w][x][y] = 0;
+                            }
                         }
                         else
-                            next[z][x][y] = 0;
+                        {
+                            if (activeNeighbors == 3)
+                            {
+                                next[z][w][x][y] = 1;
+                            }
+                            else
+                                next[z][w][x][y] = 0;
+                        }
                     }
                 }
-                cout << endl;
             }
         }
         cube = next;
@@ -98,7 +93,8 @@ int main()
     for (int z = 0; z < N; z++)
         for (int y = 0; y < N; y++)
             for (int x = 0; x < N; x++)
-                activeCounter += cube[z][x][y];
+                for (int w = 0; w < N; w++)
+                    activeCounter += cube[z][w][x][y];
 
-    cout << "the answer to part 1 is: " << activeCounter << endl;
+    cout << "the answer to part 2 is: " << activeCounter << endl;
 }
